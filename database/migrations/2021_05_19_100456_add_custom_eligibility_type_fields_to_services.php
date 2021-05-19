411987b9b09ca33e34e71776be54564972e85834
Fred Spooner <fred@aghq.uk>
2021-05-19 11:29:35 +0100

    CH-301: chore - composer fix:style

--- database/migrations/2021_05_19_100456_add_custom_eligibility_type_fields_to_services.php
+++ database/migrations/2021_05_19_100456_add_custom_eligibility_type_fields_to_services.php
@@ -8,8 +8,6 @@ class AddCustomEligibilityTypeFieldsToServices extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
@@ -26,8 +24,6 @@ class AddCustomEligibilityTypeFieldsToServices extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
