d256717ca55d279aa1fd53dcc40f5ab6d6b22d48
Fred Spooner <fred@aghq.uk>
2021-05-25 16:33:20 +0100

    CH-303: test: TaxonomyServiceEligibilityTest accesses correct fields on entity and implements new ServiceFactory states

--- tests/Feature/TaxonomyServiceEligibilityTest.php
+++ tests/Feature/TaxonomyServiceEligibilityTest.php
@@ -3,9 +3,6 @@
 namespace Tests\Feature;
 
 use App\Models\Service;
-use App\Models\SocialMedia;
-use App\Models\Taxonomy;
-use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class TaxonomyServiceEligibilityTest extends TestCase
@@ -25,28 +22,28 @@ class TaxonomyServiceEligibilityTest extends TestCase
         $response->assertJsonFragment([
             'eligibility_types' => [
                 'age_group' => [
-                    'taxonomies' => $service->serviceEligibilities['age_group'],
-                    'custom' => $service->service_eligibility_age_group_custom,
+                    'taxonomies' => $service->serviceEligibilities['age_group']['taxonomies'],
+                    'custom' => $service->eligibility_age_group_custom,
                 ],
                 'disability' => [
-                    'taxonomies' => $service->serviceEligibilities['disability'],
-                    'custom' => $service->service_eligibility_disability_custom,
-                ],
-                'gender' => [
-                    'taxonomies' => $service->serviceEligibilities['gender'],
-                    'custom' => $service->service_eligibility_gender_custom,
-                ],
-                'income' => [
-                    'taxonomies' => $service->serviceEligibilities['income'],
-                    'custom' => $service->service_eligibility_income_custom,
-                ],
-                'language' => [
-                    'taxonomies' => $service->serviceEligibilities['language'],
-                    'custom' => $service->service_eligibility_language_custom,
+                    'taxonomies' => $service->serviceEligibilities['disability']['taxonomies'],
+                    'custom' => $service->eligibility_disability_custom,
                 ],
                 'ethnicity' => [
-                    'taxonomies' => $service->serviceEligibilities['ethnicity'],
-                    'custom' => $service->service_eligibility_ethnicity_custom,
+                    'taxonomies' => $service->serviceEligibilities['ethnicity']['taxonomies'],
+                    'custom' => $service->eligibility_ethnicity_custom,
+                ],
+                'gender' => [
+                    'taxonomies' => $service->serviceEligibilities['gender']['taxonomies'],
+                    'custom' => $service->eligibility_gender_custom,
+                ],
+                'income' => [
+                    'taxonomies' => $service->serviceEligibilities['income']['taxonomies'],
+                    'custom' => $service->eligibility_income_custom,
+                ],
+                'language' => [
+                    'taxonomies' => $service->serviceEligibilities['language']['taxonomies'],
+                    'custom' => $service->eligibility_language_custom,
                 ],
             ]
         ]);
@@ -55,26 +52,14 @@ class TaxonomyServiceEligibilityTest extends TestCase
     private function createService()
     {
         $service = factory(Service::class)
-            ->states('withOfferings', 'withUsefulInfo', 'withSocialMedia')
+            ->states(
+                'withOfferings',
+                'withUsefulInfo',
+                'withSocialMedia',
+                'withCustomEligibilities',
+                'withEligibilityTaxonomies'
+            )
             ->create();
-
-        // Loop through each top level child of service eligibility taxonomy
-        Taxonomy::serviceEligibility()->children->each((function($topLevelChild) use ($service) {
-            // And for each top level child, attach one of its children to the service
-            $service->serviceEligibilities()->create([
-                'taxonomy_id' => $topLevelChild->children->first()->id,
-            ]);
-        }));
-
-        $service->eligibility_age_group_custom = 'custom age group';
-        $service->eligibility_disability_custom = 'custom disability';
-        $service->eligibility_employment_custom = 'custom employment';
-        $service->eligibility_gender_custom = 'custom gender';
-        $service->eligibility_housing_custom = 'custom housing';
-        $service->eligibility_income_custom = 'custom income';
-        $service->eligibility_language_custom = 'custom language';
-        $service->eligibility_ethnicity_custom = 'custom ethnicity';
-        $service->eligibility_other_custom = 'custom other';
 
         $service->save();
         return $service;
