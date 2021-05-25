c5033d5bccd5b49352e970fb0c334d8411971166
Fred Spooner <fred@aghq.uk>
2021-05-25 13:43:52 +0100

    CH-303: test: add service eligibility taxonomy and custom fields as factory afterCreatingState methods

--- database/factories/ServiceFactory.php
+++ database/factories/ServiceFactory.php
@@ -2,6 +2,7 @@
 
 use App\Models\Service;
 use App\Models\SocialMedia;
+use App\Models\Taxonomy;
 use Faker\Generator as Faker;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Str;
@@ -66,3 +67,25 @@ $factory->afterCreatingState(Service::class, 'withSocialMedia', function (Servic
     ]);
 });
 
+$factory->afterCreatingState(Service::class, 'withCustomEligibilities', function (Service $service, Faker $faker) {
+    $service->eligibility_age_group_custom = 'custom age group';
+    $service->eligibility_disability_custom = 'custom disability';
+    $service->eligibility_employment_custom = 'custom employment';
+    $service->eligibility_gender_custom = 'custom gender';
+    $service->eligibility_housing_custom = 'custom housing';
+    $service->eligibility_income_custom = 'custom income';
+    $service->eligibility_language_custom = 'custom language';
+    $service->eligibility_ethnicity_custom = 'custom ethnicity';
+    $service->eligibility_other_custom = 'custom other';
+    $service->save();
+});
+
+$factory->afterCreatingState(Service::class, 'withEligibilityTaxonomies', function (Service $service) {
+    // Loop through each top level child of service eligibility taxonomy
+    Taxonomy::serviceEligibility()->children->each((function($topLevelChild) use ($service) {
+        // And for each top level child, attach one of its children to the service
+        $service->serviceEligibilities()->create([
+            'taxonomy_id' => $topLevelChild->children->first()->id,
+        ]);
+    }));
+});
