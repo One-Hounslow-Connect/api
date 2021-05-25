f82b5d38ad2c9eb70d9167be7e57fbb7682a970f
Fred Spooner <fred@aghq.uk>
2021-05-25 16:31:31 +0100

    CH-303: feat: ServiceResource returns ServiceEligibilityResource, ServiceEligibilityResource returns data as array

--- app/Http/Resources/ServiceEligibilityResource.php
+++ app/Http/Resources/ServiceEligibilityResource.php
@@ -14,6 +14,6 @@ class ServiceEligibilityResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return $this->resource->toArray();
     }
 }
