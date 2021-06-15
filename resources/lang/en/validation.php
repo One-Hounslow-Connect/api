975254e6b3acad3b6dc88bad827bf4f183e55f46
Fred Spooner <fred@aghq.uk>
2021-06-15 11:19:41 +0100

    CH-181: docs: fix accidental deletion of line in comments

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -134,7 +134,8 @@ return [
     |--------------------------------------------------------------------------
     | Custom Validation Attributes
     |--------------------------------------------------------------------------
-    |    | The following language lines are used to swap attribute place-holders
+    |
+    | The following language lines are used to swap attribute place-holders
     | with something more reader friendly such as E-Mail Address instead
     | of "email". This simply helps us make messages a little cleaner.
     |
